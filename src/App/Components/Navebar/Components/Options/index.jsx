3cf928f8dd2daf090521aa0d98c1b7d5b76188a3
zeroPointField <sahiljarial1999@gmail.com>
2020-01-06 16:15:42 +0530

    Added others components

--- src/App/Components/Navebar/Components/Options/index.jsx
+++ src/App/Components/Navebar/Components/Options/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './styles/options.css';
+import userpic from './resources/userpic.jpg';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const NavOptions=()=>{
+  return (
+    <div className="naveOptions">
+     <span className="opt_span btn-primary">Home</span>
+     <span className="opt_span btn-primary">Account</span>
+      <img src={userpic} className="userPhoto"  alt="userPhoto" />
+    </div>
+  );
+};
 
 export const Options =()=>{
     return (
         <div className="options">
-          options
+          <NavOptions />
         </div>
     );
 };
