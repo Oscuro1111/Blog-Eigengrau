ee314606461637a4f384ea60ba4808a879606666
zeroPointField <sahiljarial1999@gmail.com>
2020-01-12 01:00:48 +0530

    Added pot component

--- src/App/Components/Posts/index.jsx
+++ src/App/Components/Posts/index.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
-
+import '@fortawesome/fontawesome-free/css/all.css';
 import './styles/Posts.css';
 import postPic from './resources/js.png';
 
 export const Post =()=>{
 
     return (
-          <div className="post__">
-            <div className="postTitle">Title</div>
-        
-              <span className="authorPic">
-                <img src={postPic} className="authorImage" alt="" />
-                <span className="card-title">
-                    Oscuro Smith
+        <div className="postComponent">
+          <div className="postImage">
+            <img src={postPic} alt="img"/>
+          </div>
+          <div className="postBody">
+            <div className="postTitle">Title of the post</div>
+            <div className="userInfo">
+                 <img src={postPic} className="authorImage" alt="authorPic"/>
+                 <span className="authorName">By: oscuro smith</span>
+               <span className="likesComments"> 
+                 <span className='likes'>
+                 <span className="numLikes">0</span>
+                 <span className="fas fa-thumbs-up"></span>
+               </span>               
+                <span className='comments'>
+                  <span className="numComments">0</span> 
+                  <span className="far fa-comment"></span>
                 </span>
-              </span>
-              <span className="lastUpdate">lastUpdate</span>
-              <span className="likes">
-                <span>0</span>
-                <span>Likes</span>
-              </span>
+                </span>
             </div>
+          </div>
+        </div>          
     );
 }
