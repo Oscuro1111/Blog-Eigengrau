c045976582e5f6523e9cf4b24f54e2cc886345ae
zeroPointField <sahiljarial1999@gmail.com>
2020-01-14 18:12:43 +0530

    Added post Component

--- src/App/Components/Posts/index.jsx
+++ src/App/Components/Posts/index.jsx
@@ -3,19 +3,20 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './styles/Posts.css';
 import postPic from './resources/js.png';
 
-export const Post =()=>{
+const PostHead=()=>{
 
-    return (
-        <div className="postComponent">
-          <div className="postImage">
-            <img src={postPic} alt="img"/>
-          </div>
-          <div className="postBody">
-            <div className="postTitle">Title of the post</div>
-            <div className="userInfo">
-                 <img src={postPic} className="authorImage" alt="authorPic"/>
-                 <span className="authorName">By: oscuro smith</span>
-               <span className="likesComments"> 
+  return (
+    <div className="postCategory">
+           <div className="postTopic">
+           Category : React-Js
+           </div>
+         </div>
+  );
+};
+
+const LikesComments=()=>{
+  return (
+    <span className="likesComments"> 
                  <span className='likes'>
                  <span className="numLikes">0</span>
                  <span className="fas fa-thumbs-up"></span>
@@ -25,8 +26,30 @@ export const Post =()=>{
                   <span className="far fa-comment"></span>
                 </span>
                 </span>
+  );
+};
+
+const PostBody=()=>{
+  return (
+    <div className="postBody">
+            <div className="postTitle">React functional component and higher order component</div>
+            <div className="userInfo">
+                 <img src={postPic} className="authorImage" alt="authorPic"/>
+                 <span className="authorName">By: oscuro smith</span>
+                 <LikesComments />
             </div>
           </div>
+  );
+};
+
+
+
+export const Post =()=>{
+
+    return (
+        <div className="postComponent">
+            <PostHead />
+            <PostBody />
         </div>          
     );
 }
