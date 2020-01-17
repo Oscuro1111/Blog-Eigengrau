05ce7c60c20b75c6805c1a92180dd465ff29dab4
zeroPointField <sahiljarial1999@gmail.com>
2020-01-17 16:18:58 +0530

    Comment component fully assembled + mobile view responsive issu fixed

--- src/App/Components/Posts/Components/postViewComponent.jsx
+++ src/App/Components/Posts/Components/postViewComponent.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './styles/postView.css';
+import './styles/commentComponent.css';
 import pic from '../resources/js.png';
 
 const PostHeader = () => {
@@ -33,27 +35,54 @@ const InputComment = () => {
 
 //Comment Component
  
-const CommmentHeader = () => {
+const CommentHeader = () => {
   return (
     <div className="commentHeader">
-          <div className="comment">
-		  </div>  
+	  <div className="c_userInfo">
+	  <img src={pic} className="c_authorImg" alt="user"/>  Oscuro Smith
+	  </div>
+	  <div className="c_date">
+	   Date: Jan 17 ,2020
+	  </div>
     </div>);
 };
-const CommentBody = () => {};
-const CommentFooter = () => {};
+
+const CommentBody = () => {
+	return (
+	<div className="c_body">
+	  <p>This post is amazing.</p>
+	</div>);
+};
+const CommentFooter = () => {
+	return (
+		<div className="c_footer">		   
+                 <span className='c_likes'>
+                 <span className="badge badge-secondary">3</span>
+                 <span className="fas fa-thumbs-up"></span>
+               </span>               
+                <span className='c_comments'>
+                  <span className="badge badge-secondary">0</span> 
+                  <span className="far fa-comment"></span>
+                </span>
+		</div>);
+};
 
 export const Comment = () => {
 	return( 
   <div className="commentComponent">
-     
+     <CommentHeader />
+	 <CommentBody />
+	 <CommentFooter />
   </div>);
 };
 
 /**********************************************/
 
 const CommentList = () => {
-	return <div className="commentList"></div>;
+	return (
+	<div className="commentList">
+	 <Comment />
+	</div>);
 };
 
 const PostComment = () => {
