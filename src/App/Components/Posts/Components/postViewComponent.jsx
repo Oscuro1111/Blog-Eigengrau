32fea3fc454fcc67a7436b2f98c34984feb090f6
zeroPointField <sahiljarial1999@gmail.com>
2020-01-20 01:23:36 +0530

    feature->reply+

--- src/App/Components/Posts/Components/postViewComponent.jsx
+++ src/App/Components/Posts/Components/postViewComponent.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/postView.css';
 import './styles/commentComponent.css';
 import pic from '../resources/js.png';
+import {ReplyList} from './replyComponent'; 
 
 const PostHeader = () => {
 	return (
@@ -34,55 +35,73 @@ const InputComment = () => {
 };
 
 //Comment Component
- 
+
 const CommentHeader = () => {
-  return (
-    <div className="commentHeader">
-	  <div className="c_userInfo">
-	  <img src={pic} className="c_authorImg" alt="user"/>  Oscuro Smith
-	  </div>
-	  <div className="c_date">
-	   Date: Jan 17 ,2020
-	  </div>
-    </div>);
+	return (
+		<div className="commentHeader">
+			<div className="c_userInfo">
+				<img src={pic} className="c_authorImg" alt="user" /> Oscuro Smith
+			</div>
+			<div className="c_date">Date: Jan 17 ,2020</div>
+		</div>
+	);
 };
 
 const CommentBody = () => {
 	return (
-	<div className="c_body">
-	  <p>This post is amazing.</p>
-	</div>);
+		<div className="c_body">
+			<p>This post is amazing.</p>
+		</div>
+	);
 };
+
+//*******************************ReplyComponent************************************************* */
 const CommentFooter = () => {
+	const [state, setState] = useState({ showReply: false });
+
+	const onClick = e => {
+		setState({ showReply: !state.showReply });
+	};
+
 	return (
-		<div className="c_footer">		   
-                 <span className='c_likes'>
-                 <span className="badge badge-secondary">3</span>
-                 <span className="fas fa-thumbs-up"></span>
-               </span>               
-                <span className='c_comments'>
-                  <span className="badge badge-secondary">0</span> 
-                  <span className="far fa-comment"></span>
-                </span>
-		</div>);
+		<React.Fragment>
+			<div className="c_footer">
+				<span className="c_likes">
+					<span className="badge badge-secondary">3</span>
+					<span className="fas fa-thumbs-up"></span>
+				</span>
+				<span className="c_comments" onClick={onClick}>
+					<span className="badge badge-secondary">0</span>
+					<span className="far fa-comment" ></span>
+				</span>
+				<span className="c_reply">
+					<span className="c_replyBtn far fa-reply"></span>
+					<span className="toolTipText">Reply</span>
+				</span>
+			</div>
+			{state.showReply && <ReplyList />}
+		</React.Fragment>
+	);
 };
 
 export const Comment = () => {
-	return( 
-  <div className="commentComponent">
-     <CommentHeader />
-	 <CommentBody />
-	 <CommentFooter />
-  </div>);
+	return (
+		<div className="commentComponent">
+			<CommentHeader />
+			<CommentBody />
+			<CommentFooter />
+		</div>
+	);
 };
 
 /**********************************************/
 
 const CommentList = () => {
 	return (
-	<div className="commentList">
-	 <Comment />
-	</div>);
+		<div className="commentList">
+			<Comment />
+		</div>
+	);
 };
 
 const PostComment = () => {
---
+++ src/App/Components/Posts/Components/replyComponent.jsx
@@ -0,0 +1,67 @@
+import React,{useState} from 'react';
+
+import './styles/replyComponent.css';
+
+import pic from '../resources/js.png';
+
+const ReplyHeader = () => {
+	return (
+		<div className="commentHeader padding-0-px">
+			<div className="c_userInfo">
+				<img src={pic} className="c_authorImg" alt="user" /> Oscuro Smith
+			</div>
+			<div className="c_date">Date: Jan 17 ,2020</div>
+		</div>
+	);
+};
+
+const ReplyBody = () => {
+	return (
+		<div className="c_body font-size-0-dot-9-rem padding-0-px">
+			<span>This post is amazing.</span>
+		</div>
+	);
+};
+
+//*******************************ReplyComponent************************************************* */
+const ReplyFooter = () => {
+	const [state, setState] = useState({ showReply: false });
+
+	const onClick = e => {
+		setState({ showReply: !state.showReply });
+	};
+
+	return (
+		<React.Fragment>
+			<div className="c_footer">
+				<span className="c_likes">
+					<span className="badge badge-secondary">3</span>
+					<span className="fas fa-thumbs-up"></span>
+				</span>
+				
+				<span className="c_reply">
+					<span className="c_replyBtn far fa-reply"></span>
+					<span className="toolTipText">Reply</span>
+				</span>
+			</div>
+		</React.Fragment>
+	);
+};
+
+ const ReplyComment = () => {
+	return (
+		<div className="replyComponent">
+			<ReplyHeader />
+			<ReplyBody />
+			<ReplyFooter />
+		</div>
+	);
+};
+
+
+export const ReplyList = () => {
+	return (
+        <div className="replyList">
+           <ReplyComment />
+    </div>);
+};
